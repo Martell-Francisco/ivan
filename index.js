2a4f87e9d0adfd0844c1d5ea066fc9ba66836db1
MARTELL-FRANCISCO <franciscomartell@att.net>
2023-08-17 15:07:32 -0500

    Addded a Player Object

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-<canvas></canvas>
+
+const canvas = document.querySelector('canvas')
+const c = canvas.getConstext('2d')
+canvas.width = window.innerWidth
+canvas.height = window.height
+console.log(canvas)
+
+class Player{
+    constructor(){
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.width = 100;
+        this.height = 100;
+    }
+    draw(){
+        c.fillRect(this.position.x, this.position.y, this.width, this.height )
+    }
+}
+const player = new Player()
+player.draw();
