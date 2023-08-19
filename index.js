a301f835c671c323a1f30b0876e47ceb67851d9c
Martell-Francisco <franciscomartell@att.net>
2023-08-19 14:04:49 -0500

    Created Player Class

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 
 const canvas = document.querySelector('canvas')
-const c = canvas.getConstext('2d')
-canvas.width = window.innerWidth
-canvas.height = window.height
-console.log(canvas)
+const c = canvas.getContext('2d')
 
-class Player{
-    constructor(){
-        this.position = {
-            x: 100,
-            y: 100
-        }
-        this.width = 100;
-        this.height = 100;
-    }
-    draw(){
-        c.fillRect(this.position.x, this.position.y, this.width, this.height )
-    }
-}
+canvas.width = 64*16
+canvas.height = 64*9
 const player = new Player()
-player.draw();
+
+const keys = {
+    w: {
+        pressed: false,
+    },
+    a: {
+        pressed: false,
+    },
+    d: {
+        pressed: false,
+    },
+}
+
+let y =100
+const height = 100
+let bottom = y+100
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'white'
+    c.fillRect(0,0,canvas.width, canvas.height)
+
+    player.velocity.x = 0
+if(keys.d.pressed){
+    player.velocity.x = 5
+}else if(keys.a.pressed){
+    player.velocity.x = -5
+}
+
+    player.draw()
+    player.update()
+}
+animate()
+
+
