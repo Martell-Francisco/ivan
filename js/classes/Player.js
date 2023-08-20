a00aa0b292a5bab62940ce5ad9a3f79f2809f7f4
Martell-Francisco <franciscomartell@att.net>
2023-08-20 02:07:22 -0500

    fixed gravity issue in players file

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -1,20 +1,23 @@
 class Player{
-    constructor(){
+    constructor({
+        collisionBlocks = []
+    }){
         this.position = {
-            x: 100,
-            y: 100,
+            x: 200,
+            y: 200,
 
         }
         this.velocity = {
             x: 0,
             y: 0,
         }
-        this.width = 100
-        this.height = 100
+        this.width = 25
+        this.height = 25
         this.gravity = 1
         this.sides = {
             bottom: this.position.y + this.height
         }
+        this.collisionBlocks = collisionBlocks
     }
     draw() {
         c.fillStyle = 'red';
@@ -22,11 +25,64 @@ class Player{
     }
     update() {
         this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-        this.sides.bottom = this.position.y+this.height
-        if(this.sides.bottom + this.velocity.y < canvas.height){
-            this.velocity.y +=this.gravity
-           // this.position.y++
-        }else this.velocity.y = 0
+        //Horizontal Collisions Checkkkkk
+        this.checkForHorizontalCollisions()
+        this.applyGravity()
+        this.checkForVerticalCollisions()
+
     }
+
+
+    checkForHorizontalCollisions(){
+        for (let i = 0; i < this.collisionBlocks.length; i++){
+            const collisionBlock = this.collisionBlocks[i]
+            //if collision exist
+            if(this.position.x <= collisionBlock.position.x + collisionBlock.width &&
+                this.position.x + this.width >= collisionBlock.position.x &&
+                this.position.y + this.height >= collisionBlock.position.y &&
+                this.position.y <= collisionBlock.position.y + collisionBlock.height){
+                //colliasion on the x axis going left
+                    if(this.velocity.x < -0){
+                    this.position.x = 
+                        collisionBlock.position.x + collisionBlock.width + 0.01
+                        break
+                }   
+                if(this.velocity.x > 0){
+                    this.position.x = 
+                        collisionBlock.position.x - this.width - 0.01
+                        break
+                }   
+            }
+        }
+    }
+
+    applyGravity(){
+        this.velocity.y +=this.gravity
+        this.position.y += this.velocity.y
+        
+    }
+
+    checkForVerticalCollisions(){
+        for (let i = 0; i < this.collisionBlocks.length; i++){
+            const collisionBlock = this.collisionBlocks[i]
+            //if a collision exist
+            if(this.position.x <= collisionBlock.position.x + collisionBlock.width &&
+                this.position.x + this.width >= collisionBlock.position.x &&
+                this.position.y + this.height >= collisionBlock.position.y &&
+                this.position.y <= collisionBlock.position.y + collisionBlock.height){
+                //collision on x axis going to left
+                    if(this.velocity.y < -0){
+                        this.velocity.y = 0
+                        this.position.y = collisionBlock.position.y + collisionBlock.height + 0.01
+                        break
+                }   
+                if(this.velocity.y > 0){
+                    this.velocity.y = 0
+                    this.position.y = collisionBlock.position.y - this.height - 0.01
+                     break
+                }   
+            }
+        }
+    }
+
 }
